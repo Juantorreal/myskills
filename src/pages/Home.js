a16a6990af14780b8ad93be9070d5d4711d3124d
Juan Torreal <juantorreal@gmail.com>
2022-05-21 17:15:12 -0300

    Home

---
+++ src/pages/Home.js
@@ -0,0 +1,67 @@
+import React , {Fragment} from "react";
+import { View,
+   Text,
+    StyleSheet,
+     TextInput,
+      Platform, 
+      TouchableOpacity 
+    } from "react-native";
+
+export default function Home(){
+  return(
+    <Fragment>
+    <View style={styles.container}>
+      <Text style={styles.title}>
+      Welcome, Juan
+      </Text>
+      <TextInput 
+      style={styles.input}
+      placeholder="New Skill"
+      placeholderTextColor="#555"/>
+      <TouchableOpacity style={styles.button} activeOpacity={.7}>
+        <Text style={styles.buttonText}>Add </Text>
+      </TouchableOpacity>
+      <Text style={[styles.title,{marginTop: 50}]}>
+        My Skills
+      </Text>
+     
+    </View>
+    </Fragment> 
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+     backgroundColor: '#121015',
+     paddingHorizontal: 30,
+     paddingVertical: 70,
+
+  },
+  title: {
+     color: '#fff',
+     fontSize: 24,
+     fontWeight: "bold"
+
+  },
+  input: {
+    backgroundColor: '#1F1E25',
+    color: '#fff',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+  },
+  button:{
+    backgroundColor:'#A370f7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+  },
+  buttonText: {
+    color:'#fff',
+    fontSize: 17,
+    fontWeight: 'bold'
+  }
+})
